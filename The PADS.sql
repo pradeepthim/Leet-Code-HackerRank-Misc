56584b1d03703681a64e66412b7234a486365079
PradeepthiM <112834135+pradeepthim@users.noreply.github.com>
2023-02-23 12:51:56 -0800

    The PADS
    Generate the following two result sets:
    
    Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
    Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
    
    There are a total of [occupation_count] [occupation]s.
    where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
    
    Note: There will be at least two entries in the table for each type of occupation.

---
+++ The PADS.sql
@@ -0,0 +1,8 @@
+select concat(Name,"(",left(Occupation,1),")") from OCCUPATIONS order by 
+Name;
+
+
+select concat("There are a total of ", count(Occupation), " ", lower(Occupation),"s.")
+from OCCUPATIONS
+group by Occupation
+order by count(Occupation), Occupation;
